28b877281fd0239b4232cade4396548d17b9d70b
Ghulam Ali <gal775322@gmail.com>
2026-09-18 16:18:05 +0500

    Add files via upload

---
+++ StudentFeeRecord.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LSMS
+{
+    public partial class StudentFeeRecord : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+            }
+        }
+
+       
+    }
+}
