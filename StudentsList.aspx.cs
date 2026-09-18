6086800afa2290d3fcdd60e14fb2b4757668a606
Ghulam Ali <gal775322@gmail.com>
2026-09-18 16:20:14 +0500

    Add files via upload

---
+++ StudentsList.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LSMS
+{
+    public partial class StudentsList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+            UnobtrusiveValidationMode=UnobtrusiveValidationMode.None;
+        }
+
+           
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
